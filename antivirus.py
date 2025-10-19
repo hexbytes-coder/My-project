6dc796402744565ca4ac589ba6ecaa43fac617ce
hex-bytes <p45396238@gmail.com>
2025-10-19 23:06:41 +0100

    worked on chat bot

---
+++ antivirus.py
@@ -0,0 +1,17 @@
+import time
+import os
+print("hello buddy")
+print("-"*20)
+protection = input("are you want me to protect your device? (yes/no): ")
+print(protection)
+if protection == 'yes':
+    os.getcwd()
+    os.system("dir/s")
+    time.sleep(2)
+else:
+    os.getcwd()
+    os.system("dir /s")
+    time.sleep(2)
+    print("fuck you bitch i don't protect you i will hack you bitch")
+    print("say goodbye to your all files")
+    #import tkinter as tk
